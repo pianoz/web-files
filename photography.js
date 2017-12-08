c1e20f2e6cac10b6cca967a07cccc3e03bd7ba3f
pianoz <rope7883@colorado.edu>
2017-12-07 17:12:25 -0700

    update

--- photography.js
+++ photography.js
@@ -3,7 +3,7 @@ window.addEventListener('load', () => {
   var photoData = [
     {
       title: 'Flatirons in the Snow',
-      description: 'to come later',
+      description: 'date, 12/3/2 /n F-stop 1.8. /n 50 mm /n film type /n some words no more than a sentance. ',
       url: 'photos/FlatironSnow.jpg'
     },
     {
@@ -17,44 +17,119 @@ window.addEventListener('load', () => {
       url: 'photos/JilaTower.jpg'
     },
     {
-      title: 'Flower at the Denver Botanical Gardens',
+      title: '3rd Flatiron in a Snowstorm',
       description: 'not here yet',
       url: 'photos/3rdFlatiron.jpg'
     },
     {
-      title: 'Flower at the Denver Botanical Gardens',
+      title: 'Hale Science',
       description: 'not here yet',
       url: 'photos/HaleInSnow.jpg'
     },
     {
-      title: 'Flower at the Denver Botanical Gardens',
+      title: '2017 Eclipse, Wyoming',
       description: 'not here yet',
       url: 'photos/Eclipse.jpg'
     },
     {
-      title: 'Flower at the Denver Botanical Gardens',
+      title: 'Longs Diamond at Sunrise',
       description: 'not here yet',
-      url: 'photos/LongsSunrisejpg'
+      url: 'photos/LongsSunrise.jpg'
     },
     {
-      title: 'Flower at the Denver Botanical Gardens',
+      title: 'Blue Lake, Uncomphagre Wilderness',
       description: 'not here yet',
       url: 'photos/BlueLake.jpg'
     },
     {
-      title: 'Flower at the Denver Botanical Gardens',
+      title: 'Firey Boulder Sunset',
       description: 'not here yet',
-      url: 'photosBoulderSunset.jpg'
+      url: 'photos/BoulderSunset.jpg'
     },
     {
-      title: 'Flower at the Denver Botanical Gardens',
+      title: 'Chasm Lake Storm',
       description: 'not here yet',
       url: 'photos/ChasmLake.jpg'
     },
     {
-      title: 'Flower at the Denver Botanical Gardens',
+      title: 'Chataqua in Snow',
       description: 'not here yet',
       url: 'photos/ChataquaSnow1.jpg'
+    },
+    {
+      title: 'Colmbines on Mt. Lincoln',
+      description: 'not here yet',
+      url: 'photos/ColumbinesLincoln.jpg'
+    },
+    {
+      title: 'The View',
+      description: 'not here yet',
+      url: 'photos/CrestoneAscent.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/CrestoneCampfire.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/BotanicalGdnFlower.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/LongsRoute.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/MtCameron.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/NickBelay.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/QuandarySummit.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/RedRocks.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/RoyalArch.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/SamRappel.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/SneffelsAscent.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/SneffelsRidge.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/SneffelsTent.jpg'
+    },
+    {
+      title: 'Flower at the Denver Botanical Gardens',
+      description: 'not here yet',
+      url: 'photos/TheJam.jpg'
     },
   ];
   function changePhotoNumberBy(diff) {
